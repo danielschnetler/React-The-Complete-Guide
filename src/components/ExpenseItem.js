f349afa15199041c86f9a3644a3276614c1594a5
Daniel <daniel.schnetler@gmail.com>
2022-05-22 11:21:49 +0200

    Created Variables to store the expense data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 5, 22);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
-    <div className='expense-item'>
-      <div>Date</div>
-      <div className='expense-item__description'><h2>Title</h2></div>
-      <div className='expense-item__price'>Amount</div>
+    <div className="expense-item">
+      <div> {expenseDate.toDateString()} </div>
+      <div className="expense-item__description">
+        <h2> {expenseTitle} </h2>
+      </div>
+      <div className="expense-item__price"> ${expenseAmount} </div>
     </div>
   );
 }
