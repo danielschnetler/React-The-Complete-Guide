8675c0c8b754137698d306ec58c906eaab6596bd
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-09 20:54:30 +0100

    Output dummy events, including a link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,6 @@ import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./Root";
 
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { id: "e1", description: "Event 1" },
+  { id: "e2", description: "Event 2" },
+  { id: "e3", description: "Event 3" },
+  { id: "e4", description: "Event 4" },
+];
+
 function EventsPage() {
   return (
     <>
       <h1>Events</h1>
+      <ul>
+        {EVENTS.map((ev) => (
+          <li id={ev.id}>
+            <Link to={`/events/${ev.id}`}>{ev.description}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
