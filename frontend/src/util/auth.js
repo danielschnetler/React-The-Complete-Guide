e08d074b67fabe3dc6e135b23088bbf13285cf19
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-10 21:35:04 +0100

    Managing token expiration

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,25 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const expiration = localStorage.getItem("expiration");
+  const expirationDate = new Date(expiration);
+  const now = new Date();
+  return expirationDate.getTime() - now.getTime();
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
