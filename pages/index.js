b58db1b14fed252236fd07877d1fa33ca9f80171
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-11 15:01:31 +0100

    Output a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+export function IndexPage() {
+  const DUMMY_MEETUPS = [
+    {
+      id: "m1",
+      title: "A First Meetup",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+      address: "Some address nr 20, 1234 Some City",
+      description: "This is a first meetup",
+    },
+    {
+      id: "m2",
+      title: "A Second Meetup",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+      address: "Some address nr 20, 1234 Some City",
+      description: "This is a second meetup",
+    },
+  ];
+
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
+}
+
+export default IndexPage;
