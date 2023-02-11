4864383d11d11161898d3ee4813205cec7c67240
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-11 17:26:22 +0100

    Set up mongodb for post requests

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,19 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://meetupAPI:TyWphpzxKF6Pl5bR@cluster0.l6so0lf.mongodb.net/meetups"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
