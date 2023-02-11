8808b14fa390db1c34ed787b32e0c4f61bdb0c05
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-11 16:10:27 +0100

    Data fetching for static pages

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,41 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-export function IndexPage() {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some address nr 20, 1234 Some City",
-      description: "This is a first meetup",
-    },
-    {
-      id: "m2",
-      title: "A Second Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some address nr 20, 1234 Some City",
-      description: "This is a second meetup",
-    },
-  ];
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address nr 20, 1234 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address nr 20, 1234 Some City",
+    description: "This is a second meetup",
+  },
+];
 
+export function IndexPage(props) {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={props.meetups} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  //has to be called getStaticProps() and only applys to pages in the pages folder
+
+  //Fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default IndexPage;
