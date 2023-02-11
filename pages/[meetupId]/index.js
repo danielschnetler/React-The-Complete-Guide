f2016098a5a879663f802424ea6879f43f27f7e9
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-11 16:38:26 +0100

    Preparing paths with getStaticPaths() and fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,38 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 
-function MeetupDetailsPage() {
+function MeetupDetailsPage(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="A first meetup"
-      address="Some street 5, some city"
-      description="The meetup description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  //fetch data for single meetup
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "A first meetup",
+        address: "Some street 5, some city",
+        description: "The meetup description",
+      },
+    },
+  };
 }
 
 export default MeetupDetailsPage;
