7306035f6d9fd502427f0a873e5e7387be060d28
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-12 16:56:35 +0100

    Bonus: Multiple state slices with same Custom Store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import configureStore from "./hooks-store/products-store";
+import configureCounterStore from "./hooks-store/counter-store";
 
 configureStore();
+configureCounterStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
