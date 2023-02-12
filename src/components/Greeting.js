82fe27529233102c7d4915be92139f19b10be026
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-12 18:49:02 +0100

    Another test

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Output from "./Output";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      <Output> {!changedText ? "It's good to see you!" : "Changed!"} </Output>
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,31 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("Hello World Text exists", () => {
-  //Arrange
-  render(<Greeting />);
-  //Act
-  //Assert
-  const heading = screen.getByText("Hello World", { exact: false });
-  expect(heading).toBeInTheDocument();
+describe("Greeting Component", () => {
+  test("Hello World Text exists", () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    //Assert
+    const heading = screen.getByText("Hello World", { exact: false });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Original text exists and changes when button is pressed", () => {
+    //Arange
+    render(<Greeting />);
+    //Act
+    //Assert
+    const paragraph = screen.getByText("It's good to see you!");
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).not.toHaveTextContent("Changed!");
+
+    //Act
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    //Assert
+    expect(paragraph).not.toHaveTextContent("It's good to see you!");
+    expect(paragraph).toHaveTextContent("Changed!");
+  });
 });
