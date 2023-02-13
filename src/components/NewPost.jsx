363d980dbff7c07d32cf341e0ba84cbe77f9618a
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-13 18:27:30 +0100

    Lifting State up

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,35 +1,17 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [textArea, setTextArea] = useState(""); //const something = useState(initial value); something[0]=(initial value)...; something[1](somevalue)
-  const [textInput, setTextInput] = useState("");
-
-  const textAreaChangeHandler = (event) => {
-    setTextArea(event.target.value);
-  };
-
-  function inputChangeHandler(event) {
-    setTextInput(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea
-          id="body"
-          required
-          rows={3}
-          onChange={textAreaChangeHandler}
-        />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{textArea}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={inputChangeHandler} />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
-      <p>{textInput}</p>
     </form>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("Hello");
+  const [enteredAuthor, setEnteredAuthor] = useState("Daniel");
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onAuthorChange={authorChangeHandler}
+        onBodyChange={bodyChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Daniel" body={"React is awesome!"} />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author=":)" body="Check out the full course!" />
       </ul>
     </>
