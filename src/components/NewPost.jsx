7e4b0cad32b494ee7bb2f9e68f30519e12d9945e
Daniel Schnetler <daniel.schnetler@outlook.com>
2023-02-13 18:09:11 +0100

    Working with State

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [textArea, setTextArea] = useState(""); //const something = useState(initial value); something[0]=(initial value)...; something[1](somevalue)
+  const [textInput, setTextInput] = useState("");
+
+  const textAreaChangeHandler = (event) => {
+    setTextArea(event.target.value);
+  };
+
+  function inputChangeHandler(event) {
+    setTextInput(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea
+          id="body"
+          required
+          rows={3}
+          onChange={textAreaChangeHandler}
+        />
       </p>
+      <p>{textArea}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={inputChangeHandler} />
       </p>
+      <p>{textInput}</p>
     </form>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Daniel" body={"React is awesome!"} />
-      <Post author=":)" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Daniel" body={"React is awesome!"} />
+        <Post author=":)" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
