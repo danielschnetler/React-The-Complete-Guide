45355c654382219d28ce4d2e5e6518b14e437e8c
Daniel Schnetler <daniel.schnetler@gmail.com>
2024-01-27 02:13:08 +0100

    useReducer for state

--- src/store/shopping-cart-context.tsx
+++ src/store/shopping-cart-context.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, ReactNode } from "react";
+import { createContext, ReactNode, useReducer } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products";
 
 export interface ICartItem {
@@ -22,71 +22,81 @@ export const CartContext = createContext<ICartContext>({
 interface ICartContextProvider {
   children: ReactNode;
 }
+
+function shoppingCartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product!.title,
+        price: product!.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+    };
+  }
+}
+
 const CartContextProvider: React.FC<ICartContextProvider> = ({ children }) => {
-  const [shoppingCart, setShoppingCart] = useState<ICartContext>({
+  const [cartState, cartDispatch] = useReducer(shoppingCartReducer, {
     items: [],
     addItemToCart: () => {},
     updateItemQuantity: () => {},
   });
 
   function handleAddItemToCart(id: string) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product!.title,
-          price: product!.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
+    cartDispatch({ type: "ADD_ITEM", payload: id });
   }
 
   function handleUpdateCartItemQuantity(productId: string, amount: number) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
+    cartDispatch({ type: "UPDATE_ITEM", payload: { productId, amount } });
   }
 
   const contextValue = {
-    items: shoppingCart.items,
+    items: cartState.items,
     addItemToCart: handleAddItemToCart,
     updateItemQuantity: handleUpdateCartItemQuantity,
   };
