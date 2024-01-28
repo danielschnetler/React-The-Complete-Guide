d554e83b70e17ee3c8843d8db7a7bb75feb26ec8
Daniel Schnetler <daniel.schnetler@gmail.com>
2024-01-28 14:48:36 +0100

    isPrime in memo

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useState } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 
 import IconButton from "../UI/IconButton";
 import MinusIcon from "../UI/Icons/MinusIcon";
@@ -28,7 +28,10 @@ const Counter = memo(function Counter({
   initialCount: number;
 }) {
   log("<Counter /> rendered", 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
   const [counter, setCounter] = useState(initialCount);
 
