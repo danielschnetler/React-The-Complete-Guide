6ddf7467690d90d327814bcf94dede916ecc69a4
Daniel Schnetler <daniel.schnetler@gmail.com>
2024-01-29 22:09:35 +0100

    starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-const App: React.FC = () => {
-  return <>Hello World</>;
-};
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
 
 export default App;
